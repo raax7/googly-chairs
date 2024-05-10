ab6e96387df8c9b13bb47b5844fa92c24f02513a
Toxy121 <109039201+Toxy121@users.noreply.github.com>
2024-05-10 17:35:57 -0400

    Added batch script

---
+++ Beyond Loader.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if "%~1"=="" (
+    echo Usage: "%~0" [path_to_loader]
+    pause
+    goto End
+)
+
+if not exist "%~1" (
+    echo File "%~1" not found
+    pause
+    goto End
+)
+
+start "" "%~1" zwyd1q0kqdgo7msjx0smkpg6 s5196fww29cz6kd8q6t2zxrv 1si1w8rkwig2ppa4u55bungmopqpwzruwh83gbqy56wlp12jy5fffws8pwhd6xnisnqyjgiu opqpwzruwh83gbqy56wlp12j
+
+:End
